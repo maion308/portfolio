8223b202b9e0507eb3372f7e3a6d2b7dd4161484
Michae Ion <michael.ion.mi@outlook.com>
2023-11-13 18:19:46 -0500

    initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+      <section className="header-section">
+      <header className="header">
+        <h1 className="header-h1">My Portfolio</h1>
+      </header>
+      </section>
+      <section className="main-section">
+      <main className="main-container">
+        <h3>Main section</h3>
+      </main>
+      </section>
+      <section className="footer-section">
+        <footer className="footer">
+          <p>Footer</p>
+        </footer>
+      </section>
+    </>
+  )
+}
+
+export default App
